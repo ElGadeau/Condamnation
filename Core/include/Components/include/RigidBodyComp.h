a9cba0f619639c738df3d7d541ce149a78d7e835
Victor JORE <v.jore@student.isartdigital.com>
2019-05-21 14:12:06 -0400

    bullet deleted

--- Core/include/Components/include/RigidBodyComp.h
+++ Core/include/Components/include/RigidBodyComp.h
@@ -1,16 +1,15 @@
 #pragma once
 #include <Components/include/Component.h>
-#include <PhysX/RigidBody.h>
 
 namespace Components
 {
     class RigidBodyComp : public Component
     {
     public:
-        RigidBodyComp() : m_rigidbody(new Core::PhysX::RigidBody) {};
-        ~RigidBodyComp() { delete m_rigidbody; };
+        RigidBodyComp() {};//: m_rigidbody(new Core::PhysX::RigidBody) {};
+        ~RigidBodyComp() {}; //delete m_rigidbody; };
 
-        Core::PhysX::RigidBody* m_rigidbody;
+        //Core::PhysX::RigidBody* m_rigidbody;
     };
 }
 
