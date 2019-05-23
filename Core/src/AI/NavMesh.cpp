f8f5ca41687a61c4af95d512f837f6b10557291f
Lilian <lilgado@hotmail.ca>
2019-05-23 10:05:19 -0400

    create NavMesh class

---
+++ Core/src/AI/NavMesh.cpp
@@ -0,0 +1,14 @@
+#include <AI/NavMesh.h>
+
+Physics::AI::NavMesh::NavMesh()
+{
+}
+
+Physics::AI::NavMesh::~NavMesh()
+{
+}
+
+void Physics::AI::NavMesh::FindNextPosition(glm::vec3& p_actualPosition,
+    glm::vec3& p_target)
+{
+}
