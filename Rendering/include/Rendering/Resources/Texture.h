3d07b7d855acd1befa9ac8f723cba77ecb1f9b01
Christian jesus PEREDA RIVES <c.peredarives@student.isartdigital.com>
2019-05-31 12:05:46 -0400

    class texture.cpp and texture.h are better made for implementation now, i will continue at home

--- Rendering/include/Rendering/Resources/Texture.h
+++ Rendering/include/Rendering/Resources/Texture.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <Rendering/Export.h>
 #include <Rendering/Context/IWindow.h>
 
@@ -7,11 +6,15 @@ class API_RENDERING Texture
 {
 public:
 	Texture();
-	 ~Texture();
-	void Bind();
+	~Texture();
+	void Bind(unsigned int slot = 0)const;
 	void Unbind();
-	void LoadTexture(const std::string& fileName);
-private:
+	int GetWidth() const { return m_Width; }
+	int GetHeight() const { return m_Height; }
+	void LoadTexture(const std::string& Filename);
 
+private:
 	GLuint m_texture;
+	unsigned char* m_data;
+	int m_Width, m_Height, m_BPP;
 };
