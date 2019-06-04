2925bef3c9f0d0583b153d307d5c6e1aaee8f177
Lilian <lilgado@hotmail.ca>
2019-06-04 16:01:58 -0400

    update textUI class so now it loads an entire map of characters

--- Core/src/GUI/TextUI.cpp
+++ Core/src/GUI/TextUI.cpp
@@ -18,16 +18,53 @@ void GUI::TextUI::LoadFont()
 
     FT_Set_Pixel_Sizes(face, 0, 48);
 
-    if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
-        std::cout << "ERROR::FREETYPE: failed to load Glyph\n";
+    // if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+    //     std::cout << "ERROR::FREETYPE: failed to load Glyph\n";
 
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
     for (GLubyte c = 0; c < 128; c++)
     {
         //load character Glyph
-        if (FT_Load_Char())
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYPE: failed to load Glyph\n";
+            continue;
+        }
 
+        //generate texture
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
 
+        //set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        //now store the texture for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            face->glyph->advance.x
+        };
+
+        Characters.insert(std::pair<GLchar, Character>(c, character));
     }
+
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
 }
